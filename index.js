050535c866d000a878e0bbb95de746064cc34c29
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-03-04 23:47:39 +0200

    Routing Users Requests
    הקוד מייבא את הנתב user מתיקיית Routers ורושם אותו באפליקציית Express כך שכל הנתיבים יתחילו ב-/U.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ let db_M = require('./database');
 global.db_pool = db_M.pool;
 
 
+const Users_R = require('./Routers/user');
+app.use('/U', Users_R);
 
 app.listen(port, () => {
     console.log(`Now listening on port http://localhost:${port}`);
