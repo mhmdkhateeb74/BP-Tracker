69d1548915d42bec23c9776a01cec3c936f18198
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-03-04 23:52:40 +0200

    Addmeasurements
    הפונקציה האסינכרונית Addmeasurements מכניסה נתוני לחץ דם ודופק למסד הנתונים לאחר אימות הנתונים מהבקשה. היא משתמשת ב-MySQL עם מאגר מבוסס הבטחות, מטפלת בשגיאות, ומעבירה את התוצאה למידלוור הבא.

---
+++ Middleware/measurements_Mjd.js
@@ -0,0 +1,36 @@
+async function Addmeasurements(req, res, next) {
+    let user_id   = req.body.user_id;
+    let systolic  = req.body.systolic;
+    let diastolic = req.body.diastolic;
+    let pulse     = req.body.pulse;
+    let date      = req.body.date;
+
+
+
+    if (
+        user_id === undefined ||
+        systolic === undefined ||
+        diastolic === undefined ||
+        pulse === undefined ||
+        date === undefined
+    ) {
+        req.success = false;
+        req.err = "One or more required fields are undefined";
+        return next();
+    }
+
+    const Query = `INSERT INTO measurements (user_id, systolic, diastolic, pulse, date) VALUES ('${user_id}', '${systolic}', '${diastolic}', '${pulse}', '${date}')`;
+    const promisePool = db_pool.promise();
+    let rows = [];
+    try {
+        [rows] = await promisePool.query(Query);
+        req.success = true;
+        req.insertId = rows.insertId;
+    } catch (err) {
+        console.log("Error in Addmeasurements:", err);
+        req.success = false;
+        req.err = err;
+        req.insertId = -1;
+    }
+    next();
+}
