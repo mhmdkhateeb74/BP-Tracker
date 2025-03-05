445a4ed0a226855a63bfc820c69a47773de12ff7
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-03-05 16:00:43 +0200

    Router Update/Delete
    PUT /users → מעדכן את שם המשתמש לפי המזהה שלו. מחזיר 200 OK אם הפעולה הצליחה, או 500 במקרה של שגיאה.
    DELETE /users → מוחק משתמש ממסד הנתונים לפי מזהה. מחזיר 200 OK אם הפעולה הצליחה, או 500 במקרה של שגיאה.

--- Router/user.js
+++ Router/user.js
@@ -23,6 +23,24 @@ router.post('/users',[user_Mid.AddUser], (req, res) => {
 });
 
 
+router.put('/users',[user_Mid.UpdateUser], (req, res) => {
+    if(req.success){
+        res.status(200).json({msg:"ok"});
+    } else {
+        return res.status(500).json({message:req.err});
+    }
+
+});
+
+
+router.delete('/users',[user_Mid.DeleteUser], (req, res) => {
+    if(req.success){
+        res.status(200).json({msg:"ok"});
+    } else {
+        return res.status(500).json({message:req.err});
+    }
+
+});
 
 
 module.exports = router;
