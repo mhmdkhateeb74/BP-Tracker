b4a4491e33895f5169a0e44663a0d58aa29a9952
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-03-05 15:54:41 +0200

    DeleteUser
    הפונקציה DeleteUser מוחקת משתמש ממסד הנתונים לפי המזהה (idx) שמתקבל בבקשה. אם המחיקה מצליחה, היא מסמנת req.success כ-true; אחרת, היא מדפיסה את השגיאה וממשיכה למידלוור הבא.

--- Middleware/user_Mjd.js
+++ Middleware/user_Mjd.js
@@ -58,6 +58,22 @@ async function UpdateUser(req,res,next){
     next();
 }
 
+async function DeleteUser(req,res,next){
+    let idx    = parseInt(req.body.idx);
+    let Query = `DELETE FROM users  `;
+    Query += ` WHERE id = ${idx} `;
+    // console.log(Query);
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        req.success=true;
+    } catch (err) {
+        req.success=false;
+        console.log(err);
+    }
+    next();
+}
 
 module.exports = {
     ReadUsers,
