046c8cfa269be323e5ff241d407b483f4c1d4a3f
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-03-05 15:52:08 +0200

    AddUser
    הפונקציה AddUser מוסיפה משתמש חדש למסד הנתונים באמצעות השם שמתקבל מגוף הבקשה. אם ההוספה מצליחה, היא מסמנת req.success כ-true ושומרת את מזהה המשתמש שנוסף ב-req.insertId; אחרת, היא מטפלת בשגיאה וממשיכה למידלוור הבא.

--- Middleware/user_Mjd.js
+++ Middleware/user_Mjd.js
@@ -1,4 +1,26 @@
 
+async function AddUser(req,res,next){
+    let name   = req.body.name;
+
+    const Query = `INSERT INTO users (name) VALUES('${name}')`;
+    // console.log(Query);
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        req.success=true;
+        req.insertId=rows.insertId;
+    } catch (err) {
+        console.log(err);
+        req.success=false;
+        req.insertId=-1;
+    }
+
+    next();
+}
+
+
+
 async function ReadUsers(req, res, next) {
     const Query = `SELECT * FROM users`;
 
