ea437c03149dfc19c157c8d229ef700b24dda43a
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2025-03-06 17:41:53 +0200

    FrontRouters
     הקוד מגיש קבצים סטטיים כמו CSS ו-JavaScript מתוך תיקיות "css" ו-"js", וקבצי HTML מתוך תיקיית "view". הוא גם מגדיר נתיבים מפורשים לדף הבית (index.html), דף ההיסטוריה (history.html), ודף הסטטיסטיקות (stats.html) אשר יוגשו כאשר הגולש ייגש לנתיבים אלה.

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const bodyParser = require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/js", express.static(path.join(__dirname, "js")));
+app.use(express.static(path.join(__dirname, "view")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "view", "index.html"));
+});
+
+app.get("/history", (req, res) => {
+    res.sendFile(path.join(__dirname, "view", "history.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "view", "stats.html"));
+});
+
+
 let db_M = require('./database');
 global.db_pool = db_M.pool;
 
